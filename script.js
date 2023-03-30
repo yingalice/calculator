9c7ecf4de8486d501c8c21db4f1049988848672a
Alice Ying <108079382+yingalice@users.noreply.github.com>
2023-03-29 18:09:22 -0700

    Changed to display only 1 operation at a time
    Having multiple operations in 1 line and evaluating operands from
    left to right (per project instructions) is misleading, as most
    would it expect to follow the order of operations.

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-let operation = {operand1: '', operand2: '', operator: ''};
-let display = {mainText: '0', resultText: '', cursor: 'off'};
-let history = [];
-let needsHistoryCleanup = false;
+let expression = { operand1: '0', operand2: '', operator: '' };
+let display = { mainText: '0', resultText: '', cursor: 'off' };
 
-const mainDisplay = document.querySelector('.display__main');
-const resultDisplay = document.querySelector('.display__result');
-const displayCursor = document.querySelector('.display__cursor');
+const allBtns = document.querySelectorAll('[data-btn-type]');
+const numberBtns = document.querySelectorAll('[data-btn-type="number"]');
+const operatorBtns = document.querySelectorAll('[data-btn-type="operator"]');
+const decimalBtn = document.querySelector('[data-btn-type="decimal"');
 const clearBtn = document.querySelector('[data-btn-type="clear"]');
 const backspaceBtn = document.querySelector('[data-btn-type="backspace"]');
 const equalsBtn = document.querySelector('[data-btn-type="equals"]');
-const allBtns = document.querySelectorAll('[data-btn-type]');
-const historyBtns = document.querySelectorAll('[data-btn-type="number"], [data-btn-type="decimal"], [data-btn-type="operator"]')
-const decimalBtn = document.querySelector('[data-btn-type="decimal"');
-const numberBtns = document.querySelectorAll('[data-btn-type="number"]');
-const operatorBtns = document.querySelectorAll('[data-btn-type="operator"]');
 
-document.addEventListener('keydown', handleKeyboardInput);
-allBtns.forEach(btn => btn.addEventListener('click', addBtnPressEffect));
-historyBtns.forEach(btn => btn.addEventListener('click', saveHistory));
-numberBtns.forEach(btn => btn.addEventListener('click', appendNumber));
-operatorBtns.forEach(btn => btn.addEventListener('click', appendOperator));
+allBtns.forEach((btn) => btn.addEventListener('click', addBtnPressEffect));
+numberBtns.forEach((btn) => btn.addEventListener('click', appendNumber));
+operatorBtns.forEach((btn) => btn.addEventListener('click', appendOperator));
+decimalBtn.addEventListener('click', appendDecimal);
 clearBtn.addEventListener('click', clear);
 backspaceBtn.addEventListener('click', backspace);
-equalsBtn.addEventListener('click', calculate);
-decimalBtn.addEventListener('click', appendDecimal);
+equalsBtn.addEventListener('click', displayFinalResult);
+document.addEventListener('keydown', handleKeyboardInput);
 
 function add(a, b) {
   return a + b;
@@ -38,132 +31,156 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  return (b === 0) ? `Can't divide by 0` : a / b;
+  return b === 0 ? `Can't divide by 0` : a / b;
 }
 
 function operate() {
-  let a = Number(operation.operand1);
-  let b = Number(operation.operand2);
-
-  switch (operation.operator) {
+  // Returns string answer to the math expression
+  if (!isExpressionComplete()) return;
+  let a = Number(expression.operand1);
+  let b = Number(expression.operand2);
+  switch (expression.operator) {
     case '+':
-      return roundResult(add(a, b));
+      return String(roundResult(add(a, b)));
     case '-':
-      return roundResult(subtract(a, b));
+      return String(roundResult(subtract(a, b)));
     case 'x':
-      return roundResult(multiply(a, b));
+      return String(roundResult(multiply(a, b)));
     case '÷':
-      return roundResult(divide(a, b));
+      return String(roundResult(divide(a, b)));
     default:
-      console.error(`${arguments.callee.name} - Invalid operator: ${operation.operator}`);
+      console.error(`${arguments.callee.name}() - Invalid operator '${expression.operator}'`);
+      return;
   }
-}
-
-function clear() {
-  operation.operand1 = '';
-  operation.operand2 = '';
-  operation.operator = '';
-  updateMainDisplay('0');
-  updateResultDisplay('');
-  setCursorBlink('off');
-  history = [];
-  needsHistoryCleanup = false;
-}
-
-function backspace() {
-  if (!history.length) return;
-  const lastHistory =   history.pop();
-  operation.operand1 = lastHistory.operand1;
-  operation.operand2 = lastHistory.operand2;
-  operation.operator = lastHistory.operator;
-  updateMainDisplay(lastHistory.mainText);
-  updateResultDisplay(lastHistory.resultText);
-  setCursorBlink(lastHistory.cursor);
-}
-
-function appendDecimal() {
-  const operand = getCurrentOperand();
-  if (operation[operand].includes('.')) {
-    history.pop();
-    return;
-  }
-  const decimalFormat = (operation[operand] === '' || display.mainText === '0') ? '0.' : '.';
-  operation[operand] += decimalFormat;
-  if (display.mainText === '0') display.mainText = '';
-  updateMainDisplay(`${display.mainText}${decimalFormat}`);
-  setCursorBlink('on');
 }
 
 function appendNumber(e) {
   const numberInput = e.target.textContent;
   const operand = getCurrentOperand();
-  if (display.mainText === '0' || (operation[operand].startsWith('0') && !operation[operand].includes('.'))) {
-    operation[operand] = '';
-    if (display.mainText !== '0') history.pop();
-    display.mainText = display.mainText.slice(0, -1);
+
+  if (expression[operand] === '0') expression[operand] = ''; // Remove leading zeros
+  expression[operand] += numberInput; // Append new number
+  if (operand === 'operand2') {
+    const result = operate();
+    if (!isNaN(result)) {
+      updateResultDisplay(addComma(result)); // Update calculation in real time
+    } else {
+      updateResultDisplay(''); // Don't display divide by 0 error unless they press equal
+    }
   }
-  operation[operand] += numberInput;
-  if (operand === 'operand2') updateResultDisplay(String(operate()));
-  const operandComma = addComma(operation[operand]);
-  const idx = display.mainText.lastIndexOf(' ');
-  const mainDisplayValueWithoutLastOperand = (idx >= 0) ? display.mainText.slice(0, idx) : '';
-  updateMainDisplay(`${mainDisplayValueWithoutLastOperand} ${operandComma}`);
+  updateMainDisplay(formatExpression());
   setCursorBlink('on');
 }
 
 function appendOperator(e) {
-  if (!operation.operand1) {   // 1. No numbers entered yet
-    operation.operand1 = '0';  //    Set operand1 to 0 to match starting display
-  } else if (operation.operand1 && operation.operand2 && operation.operator) {  // 2. Pair of numbers ready to be calculated
-    operation.operand1 = String(operate());                                     //    Calculate last pair of numbers to support stringing multiple operations
-    operation.operand2 = '';
-    operation.operator = '';
-  } else if (operation.operand1 && !operation.operand2 && operation.operator) {  // 3. Operators entered back-to-back
-    display.mainText = display.mainText.slice(0, -3);                            //    Remove last operator
-    history.pop();
-  }
-  operation.operator = e.target.textContent;
-  updateMainDisplay(`${display.mainText} ${operation.operator} `);
+  // Calculate any existing pair of numbers first
+  // Operators evaluated left to right (no order of expressions)
+  // Example: 12 + 7 - 5 * 3 = 42.  After inputting '12 + 7 -', main display will read '19 -'
+  if (expression.operand2) finalizeCalculation();
+  expression.operator = e.target.textContent;
+  updateMainDisplay(formatExpression());
+  updateResultDisplay('');
   setCursorBlink('on');
 }
 
-function calculate() {
-  if (display.resultText === '') return;
-  if (!isNaN(removeComma(display.resultText))) {
-    cleanupHistory();
-    history[history.length - 1].equalsPressed = true;
-    needsHistoryCleanup = true;
-    operation.operand1 = removeComma(display.resultText);
-    operation.operand2 = '';
-    operation.operator = '';
-    updateMainDisplay(display.resultText);
-    updateResultDisplay('');
-  }
+function appendDecimal() {
+  const operand = getCurrentOperand();
+  if (expression[operand].includes('.')) return; // No more than 1 decimal per operand
+  const decimalFormat = expression[operand] === '' ? '0.' : '.'; // Format decimal with leading zero (ie. 0.15 insted of .15)
+  expression[operand] += decimalFormat;
+  updateMainDisplay(formatExpression());
+  setCursorBlink('on');
+}
+
+function clear() {
+  // Reset everything
+  expression = { operand1: '0', operand2: '', operator: '' };
+  updateMainDisplay('0');
+  updateResultDisplay('');
   setCursorBlink('off');
 }
 
+function backspace() {
+  // Everything erased, reset to 0 and turn off cursor
+  if (display.mainText.length === 1) {
+    clear();
+    return;
+  }
+
+  // Remove last character, and update corresponding variable (operator1, operator2, or operand)
+  if (expression.operand2) {
+    expression.operand2 = expression.operand2.slice(0, -1);
+  } else if (expression.operator) {
+    expression.operator = '';
+  } else if (expression.operand1) {
+    expression.operand1 = expression.operand1.slice(0, -1);
+  }
+
+  updateMainDisplay(formatExpression());  // Main display without last character
+  const result = (isExpressionComplete() && !(expression.operator === '÷' && Number(expression.operand2) === 0))
+                   ? addComma(operate())
+                   : '';
+  updateResultDisplay(result);  // Only display results if it can be calculated without error
+  setCursorBlink('on');
+}
+
+function displayFinalResult() {
+  if (!isExpressionComplete()) return;
+  const result = display.resultText === '' ? operate() : finalizeCalculation();
+  if (isNaN(result)) {
+    updateResultDisplay(addComma(result));
+  } else {
+    updateMainDisplay(addComma(result));
+    updateResultDisplay('');
+    setCursorBlink('off');
+  }
+}
+
+function finalizeCalculation() {
+  // Returns result of the current expression
+  // User is done entering this expression, so use result as operand1 of the
+  // next expression, and clear out operand2 and operator
+  const result = operate();
+  expression.operand1 = result;
+  expression.operand2 = '';
+  expression.operator = '';
+  return result;
+}
+
 function updateMainDisplay(content) {
-  display.mainText = content;
+  const mainDisplay = document.querySelector('.display__main');
+  display.mainText = String(content);
   mainDisplay.textContent = display.mainText;
 }
 
 function updateResultDisplay(content) {
-  content = removeComma(content);
-  if (isNaN(content)) {
+  const resultDisplay = document.querySelector('.display__result');
+  // Divide by 0 error is shown in red, with operator and equals buttons disabled
+  if (isNaN(removeComma(content))) {
     resultDisplay.classList.add('display__result--error');
-    operatorBtns.forEach(btn => btn.disabled = true);
+    operatorBtns.forEach((btn) => (btn.disabled = true));
     equalsBtn.disabled = true;
   } else {
     resultDisplay.classList.remove('display__result--error');
-    operatorBtns.forEach(btn => btn.disabled = false);
+    operatorBtns.forEach((btn) => (btn.disabled = false));
     equalsBtn.disabled = false;
   }
-  display.resultText = addComma(content);
+  display.resultText = String(content);
   resultDisplay.textContent = display.resultText;
+}
+
+function formatExpression() {
+  // Returns operands and operator formatted into an expression
+  // Operands have comma thousands separator to be display-friendly
+  // Example: 5,000 + 0.125
+  return `${addComma(expression.operand1)} 
+          ${expression.operator} 
+          ${addComma(expression.operand2)}`.trim();
 }
 
 function setCursorBlink(status) {
   // Show or hide the blinking cursor
+  const displayCursor = document.querySelector('.display__cursor');
   switch (status) {
     case 'on':
       displayCursor.classList.add('display__cursor--blink');
@@ -172,7 +189,7 @@ function setCursorBlink(status) {
       displayCursor.classList.remove('display__cursor--blink');
       break;
     default:
-      console.error(`${arguments.callee.name} - Invalid status: ${status}`);
+      console.error(`${arguments.callee.name}() - Invalid status: ${status}`);
       return;
   }
   display.cursor = status;
@@ -189,19 +206,21 @@ function roundResult(input) {
 function addComma(input) {
   // Returns string with number formatted with comma thousands separator (ie. 4150 -> 4,150)
   input = String(input);
-  if ((!isNaN(input) && input.includes('e')) ||  // Scientific notation
-      input === '' ||                            // Blank
-      isNaN(input))                              // Can't divide by 0
-  {
-    return input;  // No change, already in desired format
+  if (
+    (!isNaN(input) && input.includes('e')) || // Scientific notation
+    input === '' || // Blank
+    isNaN(input)
+  ) {
+    // Can't divide by 0
+    return input; // No change, already in desired format
   }
-  // Convert the whole number part to have comma thousands separator, 
+  // Convert the whole number part to have comma thousands separator,
   // then combine it with the decimal part unchanged
   // Doing this because regex found online doesn't work right on decimals
   // Example: Want 4000.3127 to be 4,000.3127 (not 4,000.3,127)
   const idxOfDecimal = input.indexOf('.');
-  const numberPart = (idxOfDecimal >= 0) ? input.slice(0, idxOfDecimal) : input;
-  const decimalPart = (idxOfDecimal >= 0) ? input.slice(idxOfDecimal) : '';
+  const numberPart = idxOfDecimal >= 0 ? input.slice(0, idxOfDecimal) : input;
+  const decimalPart = idxOfDecimal >= 0 ? input.slice(idxOfDecimal) : '';
   const numberPartWithComma = numberPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   return `${numberPartWithComma}${decimalPart}`;
 }
@@ -211,51 +230,10 @@ function removeComma(input) {
   return String(input).replace(/,/g, '');
 }
 
-function saveHistory() {
-  const entry = {
-    operand1: operation.operand1,
-    operand2: operation.operand2,
-    operator: operation.operator,
-    mainText: display.mainText,
-    resultText: display.resultText,
-    cursor: display.cursor,
-    equalsPressed: false,
-  };
-  history.push(entry);
-  if (needsHistoryCleanup) cleanupHistory();
-}
-
-function cleanupHistory() {
-  needsHistoryCleanup = false;
-  const idx = history.findIndex(item => item.equalsPressed === true);
-  if (idx > 0) history.splice(1, idx);
-  if (history.length < 2) return;
-  const mainText = history[1].mainText.trim();
-  if (!isNaN(mainText) && mainText.includes('e')) return; // Scientific notation
-  if (mainText.length > 1) {
-    // Extract 1 fewer digit each time
-    // Example: 456 becomes 3 separate history entries: 456, 45, 4
-    //          to allow backspacing one number at a time
-    for (let i = 1; i <= mainText.length - 1; i++) {
-      const partialNumber = mainText.slice(0, -i)
-      const entry = {
-        operand1: partialNumber,
-        operand2: '',
-        operator: '',
-        mainText: partialNumber,
-        resultText: '',
-        cursor: 'on',
-        equalsPressed: false,
-      };
-      history.splice(1, 0, entry);  
-    }
-  }
-}
-
 function handleKeyboardInput(e) {
   // Click corresponding button upon keyboard input
   // Equals button responds to both 'Enter' and '=' keys
-  if (e.key === '/') e.preventDefault();  // prevent '/' from opening find in Firefox
+  if (e.key === '/') e.preventDefault(); // prevent '/' from opening find in Firefox
   const btn = document.querySelector(`[data-key~="${e.key}"]`);
   if (btn) btn.click();
 }
@@ -269,5 +247,11 @@ function addBtnPressEffect(e) {
 }
 
 function getCurrentOperand() {
-  return (!operation.operator) ? 'operand1' : 'operand2';
+  return !expression.operator ? 'operand1' : 'operand2';
+}
+
+function isExpressionComplete() {
+  // Returns true if all 3 expression properties are populated
+  // (operand1, operand2, operator)
+  return Object.values(expression).every((prop) => prop !== '');
 }
