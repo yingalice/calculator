fbac3026694ff203030f4cf4539876562e048c2a
Alice Ying <108079382+yingalice@users.noreply.github.com>
2023-04-08 09:36:58 -0700

    Fix infinity and clearing history issues, refactor Infinity:    - Disable appending number or decimal to Infinity    - Backspace deletes entire 'Infinity', not 1 character at a time Clearing history (timing issues due to delaying deletion to sync with fade effect):    - Let new history entry be added as current history is being cleared    - Do not allow user to select history entry that is being cleared

--- script.js
+++ script.js
@@ -55,15 +55,15 @@ function operate() {
   let b = Number(expression.operand2);
   switch (expression.operator) {
     case '+':
-      return String(roundResult(add(a, b)));
+      return roundResult(add(a, b));
     case '-':
-      return String(roundResult(subtract(a, b)));
+      return roundResult(subtract(a, b));
     case 'x':
-      return String(roundResult(multiply(a, b)));
+      return roundResult(multiply(a, b));
     case '÷':
-      return String(roundResult(divide(a, b)));
+      return roundResult(divide(a, b));
     default:
-      console.error(`${arguments.callee.name}() - Invalid operator '${expression.operator}'`);
+      console.error(`Invalid operator '${expression.operator}'`);
       return;
   }
 }
@@ -74,6 +74,7 @@ function appendNumber(e) {
   const numberInput = e.target.textContent;
   const operand = getCurrentOperand();
   
+  if (expression[operand].includes('Infinity')) return;  // Don't append numbers to Infinity
   if (expression[operand] === '0' || expression[operand] === '-0') {
     expression[operand] = expression[operand].replace('0', '');  // Remove leading zero
   }
@@ -108,25 +109,25 @@ function appendPosNeg() {
 
 function appendDecimal() {
   const operand = getCurrentOperand();
-  if (expression[operand].includes('.')) return;  // No more than 1 decimal per operand
+  if ((expression[operand].includes('.')) ||  // No more than 1 decimal per operand
+      (expression[operand].includes('Infinity'))) {  // Don't append decimals to Infinity
+        return;
+     }
   const decimalFormat = (expression[operand] === '') ? '0.' : '.';  // Format decimal with leading zero (ie. 0.15 instead of .15)
   expression[operand] += decimalFormat;  // Append decimal
   refreshDisplay();
 }
 
 function finalizeCalculation() {
-  // Returns string result of the current expression
   // User is done entering this expression (pressed equals or an additional operator)
   // 1.  Add to history panel
   // 2.  Save expression so user can undo with backspace (ie. backspace 7 reveals 5 + 2)
   // 3.  Use result as operand1 of next expression, and clear operand2 and operator
-  const result = operate();
-  appendHistory({...expression});
+  appendHistory();
   savedExpression = {...expression};
-  expression.operand1 = result;
+  expression.operand1 = removeComma(display.resultText);
   expression.operand2 = '';
   expression.operator = '';
-  return result;
 }
 
 
@@ -155,9 +156,10 @@ function backspace() {
   } else if (expression.operator) {
     expression.operator = '';
   } else if (expression.operand1) {
-    if ((display.mainText.length === 1) ||  // On last character
+    if ((expression.operand1.length === 1) ||  // On last character
        ((expression.operand1.startsWith('-')) && (expression.operand1.length === 2)) ||  // Single digit negative
-       (expression.operand1.includes('e'))) {  // Scientific notation
+       (expression.operand1.includes('e')) ||  // Scientific notation
+       (expression.operand1.includes('Infinity'))) {  // Infinity
       clear();  // Delete everything (don't backspace one-by-one).  Reset to 0
       return;
     } else {
@@ -169,39 +171,24 @@ function backspace() {
 
 
 // ========== Display ==========
-function displayFinalResult() {
-  if (!isExpressionComplete()) return;
-  // If result is currently blank, then there's a hidden error
-  // Show the error, but don't save it to operand1 or update the main display and history
-  const result = (display.resultText === '') ? operate() : finalizeCalculation();
-  if (isNaN(result)) {
-    // Result display = Shows error
-    // Main display = unchanged (still shows expression that led to error)
-    updateResultDisplay(addComma(result));
-  } else {
-    // Main display = Shows calculated result (number)
-    // Result display = blank
-    updateMainDisplay(addComma(result));
-    updateResultDisplay('');
-  }
-}
-
 function updateMainDisplay(content) {
+  // Show provided text in main display (top part)
   const mainDisplay = document.querySelector('.display__main');
   display.mainText = String(content);
   mainDisplay.textContent = display.mainText;
 }
 
 function updateResultDisplay(content) {
+  // Show provided text in result display (bottom part)
+  // Errors are shown in red, with operator and equals buttons disabled
   const resultDisplay = document.querySelector('.display__result');
-  // Divide by 0 error is shown in red, with operator and equals buttons disabled
   if (isNaN(removeComma(content))) {
     resultDisplay.classList.add('display__result--error');
-    operatorBtns.forEach((btn) => (btn.disabled = true));
+    operatorBtns.forEach((btn) => btn.disabled = true);
     equalsBtn.disabled = true;
   } else {
     resultDisplay.classList.remove('display__result--error');
-    operatorBtns.forEach((btn) => (btn.disabled = false));
+    operatorBtns.forEach((btn) => btn.disabled = false);
     equalsBtn.disabled = false;
   }
   display.resultText = String(content);
@@ -209,15 +196,15 @@ function updateResultDisplay(content) {
 }
 
 function refreshDisplay() {
-  // Refreshes the display upon new button input or history restoration,
-  // based on the operands and operator currently stored in expression
+  // Refreshes the display upon new button input or history restoration
+  // Uses the operands and operator currently stored in expression
   // Calculates the result if possible, errors are hidden at this stage
   let result = '';
   if (isExpressionComplete()) {
     result = operate();
     result = (!isNaN(result))
                ? addComma(result)  // Update calculations in real time
-               : '';               // Don't display errors yet (only shown when equals is pressed)
+               : '';               // Don't display errors yet (only show when equals is pressed)
   }
 
   updateMainDisplay(formatExpression());
@@ -229,10 +216,27 @@ function refreshDisplay() {
   savedExpression = {};
 }
 
+function displayFinalResult() {
+  if (!isExpressionComplete()) return;
+  if (display.resultText === '') {
+    // If result is currently blank, then there's a hidden error.  Show it.
+    // Main display = unchanged (still shows expression that led to error)
+    // Result display = Shows error
+    updateResultDisplay(operate());
+  } else {
+    // Main display = Shows calculated result (number)
+    // Result display = blank
+    finalizeCalculation();  // Add to history panel, save result to operand1 to use in next calculation
+    updateMainDisplay(display.resultText);
+    updateResultDisplay('');
+  }
+}
+
 
 // ========== History ==========
-function appendHistory(historyData) {
-  history.push(historyData);
+function appendHistory() {
+  // Add current expression and result to history panel
+  history.push({...expression});  // Saves operands and operator, so it can be restored
 
   const divEntry = document.createElement('div');
   const divExpression = document.createElement('div');
@@ -241,42 +245,51 @@ function appendHistory(historyData) {
   divEntry.classList.add('history__entry');
   divExpression.classList.add('history__expression');
   divResult.classList.add('history__result');
-  divExpression.textContent = `${formatExpression()} =`
+  divExpression.textContent = `${display.mainText} =`
   divResult.textContent = display.resultText;
 
-  historyEntries.insertBefore(divEntry, historyEntries.firstChild);
   divEntry.appendChild(divExpression);
   divEntry.appendChild(divResult);
-  divEntry.setAttribute('data-id', history.length - 1);
+  divEntry.dataset.id = history.length - 1;  // Save index for easy lookup of history[] item
+  historyEntries.insertBefore(divEntry, historyEntries.firstChild);  // Add new entry to top
 
-  const height = window.getComputedStyle(divEntry).height;
-  divEntry.style.setProperty('--height', height);
+  // Dynamically determine height of each entry to use in slide animation
+  const height = divEntry.offsetHeight;
+  divEntry.style.setProperty('--height', `${height}px`);
   divEntry.classList.add('history__entry--slide');
 }
 
 function selectHistoryEntry(e) {
   // Update calculator display to match the selected history entry
+  if (history.length === 0) return;  // Exit if user clicked entry as it was being cleared
   const target = e.target.closest('.history__entry');
   if (!target) return;
-  const id = Number(target.dataset.id);
+  const id = target.dataset.id;
   expression = {...history[id]};
   refreshDisplay();
 }
 
 function clearHistory() {
-  history = [];
+  // Remove all history entries
+  if (historyEntries.classList.contains('history__entries--fade')) return;
   historyEntries.classList.add('history__entries--fade');
-  [...historyEntries.children].forEach((div) => {
-    setTimeout(() => div.parentNode.removeChild(div), 1000);
-  });
-  setTimeout(() => historyEntries.classList.remove('history__entries--fade'), 1000);
+  deleteCount = history.length;
+  history = [];
+  // Only remove history that was there when button was clicked.  This makes it 
+  // possible to add a new entry while the current history is being cleared
+  setTimeout(() => {
+    for (i = 0; i < deleteCount; i++) {
+      historyEntries.removeChild(historyEntries.lastChild);
+    }
+    historyEntries.classList.remove('history__entries--fade');
+  }, 500);
 }
 
 
 // ========== Formatting ==========
 function roundResult(input) {
-  // Returns number rounded to max precision
-  if (isNaN(input)) return input;
+  // Returns string with number rounded to max precision
+  if (isNaN(input)) return String(input);
 
   // toPrecision(15) to prevent floating point calculation errors
   // parseFloat to remove trailing 0's in decimals
@@ -285,33 +298,18 @@ function roundResult(input) {
   // JavaScript uses scientific notation at 1e21
   // Change that to 1e16 so it doesn't take up too much space
   const result = (rounded >= 1e16 || rounded <= -1e16)
-                 ? Number(rounded).toExponential()
+                 ? rounded.toExponential()
                  : rounded;
-  return result;
+  return String(result);
 }
 
 function addComma(input) {
   // Returns string with number formatted with comma thousands separator (ie. 4150 -> 4,150)
-  input = String(input);
-
-  // Already in the desired format (no change):
-  if (
-    (!isNaN(input) && input.includes('e')) ||  // Scientific notation
-    input === '' ||                            // Blank
-    isNaN(input)                               // Can't divide by 0
-  ) {
-    return input;
-  }
-
   // Convert the whole number part to have comma thousands separator,
   // then combine it with the decimal part unchanged
-  // Doing this because regex found online doesn't work on decimals
-  // Example: Want 4000.3127 to be 4,000.3127 (not 4,000.3,127)
-  const idxOfDecimal = input.indexOf('.');
-  const numberPart = idxOfDecimal >= 0 ? input.slice(0, idxOfDecimal) : input;
-  const decimalPart = idxOfDecimal >= 0 ? input.slice(idxOfDecimal) : '';
-  const numberPartWithComma = numberPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  return `${numberPartWithComma}${decimalPart}`;
+  const parts = String(input).split('.');
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return parts.join('.');
 }
 
 function removeComma(input) {
@@ -320,33 +318,36 @@ function removeComma(input) {
 }
 
 function addParentheses(input) {
-  // Add parentheses for negative numbers
-  return (String(input).startsWith('-'))
-          ? `(${input})`
-          : input;
+  // Returns string with parentheses added for negative numbers
+  input = String(input);
+  return (input.startsWith('-'))
+           ? `(${input})`
+           : input;
 }
 
 function formatExpression() {
   // Returns operands and operator formatted into an expression
-  // Operands have comma thousands separator to be display-friendly
-  // Example: 5,000 + 0.125
+  // Operands have comma thousands separator added
+  // Negative numbers have parentheses added
+  // Example: 5,000 + (-0.125)
   return `${addParentheses(addComma(expression.operand1))} 
           ${expression.operator} 
-          ${addParentheses(addComma(expression.operand2))}`.trim();
+          ${addParentheses(addComma(expression.operand2))}`
+          .trim();
 }
 
 
 // ========== Keyboard ==========
 function handleKeyboardInput(e) {
   // Click corresponding button upon keyboard input
-  // Equals button responds to both 'Enter' and '=' keys
-  if (e.key === '/') e.preventDefault(); // prevent '/' from opening find in Firefox
+  // Note: Equals button responds to both 'Enter' and '=' keys
+  if (e.key === '/') e.preventDefault();  // prevent '/' from opening find in Firefox
   const btn = document.querySelector(`[data-key~="${e.key}"]`);
   if (btn) {
     btn.click();
     // Trigger button press effect for keyboard input by quickly adding/removing class
     btn.classList.add('button--pressed');
-    setTimeout(() => btn.classList.remove('button--pressed'), 100);
+    setTimeout(() => btn.classList.remove('button--pressed'), 50);
   }
 }
 
@@ -361,7 +362,7 @@ function setHistoryPanelHeight() {
   const flexDirection = window.getComputedStyle(main).flexDirection;
   history.style.height = (flexDirection === 'row')
                          ? `${calculatorHeight}px`          // Side-by-side = 100% of calculator's height
-                         : `${calculatorHeight * 0.6}px`;   // Up-and-down = 70% of calculator's height
+                         : `${calculatorHeight * 0.6}px`;   // Up-and-down = 60% of calculator's height
 }
 
 function getCurrentOperand() {
@@ -372,5 +373,5 @@ function getCurrentOperand() {
 function isExpressionComplete() {
   // Returns true if all 3 expression properties are populated
   // (operand1, operand2, operator)
-  return Object.values(expression).every((prop) => prop !== '');
+  return Object.values(expression).every((value) => value !== '');
 }
